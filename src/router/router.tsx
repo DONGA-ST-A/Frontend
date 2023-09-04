def264447f717fe6e7b4773075487f2c5b46f420
aaminha <dks4857@gmail.com>
2023-09-05 02:53:26 +0900

    chore: application path alias

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,14 @@
 import Layout from "@/layout/Layout";
+import MainPage from "@/pages/MainPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
