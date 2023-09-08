b09d9b2054e7eab507f1330b6545725edc899d71
aaminha <dks4857@gmail.com>
2023-09-09 00:33:26 +0900

    design(commonStyle): apply title css

--- src/style/commonStyle.tsx
+++ src/style/commonStyle.tsx
@@ -8,3 +8,34 @@ const Inner = styled.div`
 `;
 
 export { Inner };
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Title = styled.div`
+  margin-left: 24px;
+`;
+
+export const TitleEn = styled.div`
+  color: var(--color_sub2);
+  font-size: var(--text_body2);
+  font-family: NotoSansMedium;
+  line-height: 1.6em;
+`;
+
+export const TitleKo = styled.div`
+  color: var(--color_font);
+  font-size: var(--text_title1);
+  font-family: NotoSansBold;
+  line-height: 1.6em;
+`;
+
+export const VerticalLine = styled.div`
+  content: "";
+  background-color: var(--color_main_skyblue);
+  width: 4px;
+  border-radius: 6px;
+  height: 90px;
+`;
