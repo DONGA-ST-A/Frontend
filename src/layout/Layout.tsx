82a984544b91c56dfe54471f02e73426ec265332
aaminha <dks4857@gmail.com>
2023-09-09 00:10:39 +0900

    feat(TopButton): add top button

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "@/pages/common/Footer";
 import Header from "@/pages/common/Header";
+import { TopButton } from "@/style/commonStyle";
 
 const Layout = () => {
   return (
@@ -9,6 +10,11 @@ const Layout = () => {
       <Header />
       <Outlet />
       <Footer />
+      <TopButton
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      />
     </>
   );
 };
--- src/style/commonStyle.tsx
+++ src/style/commonStyle.tsx
@@ -1,5 +1,7 @@
 import { styled } from "styled-components";
 
+import TopArrowIcon from "@/assets/icon/icon_top_arrow.svg";
+
 const Inner = styled.div`
   max-width: 1230px;
   margin: 0 auto;
@@ -8,3 +10,19 @@ const Inner = styled.div`
 `;
 
 export { Inner };
+
+export const TopButton = styled.button`
+  background-image: url(${TopArrowIcon});
+  border: none;
+  border-radius: 50%;
+  width: 57px;
+  height: 57px;
+  background-size: cover;
+  background-position: center;
+  &:hover {
+    cursor: pointer;
+  }
+  position: fixed;
+  bottom: 40px;
+  right: 30px;
+`;
