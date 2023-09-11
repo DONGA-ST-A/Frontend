649b297f3a42695f430428e13870ecd576ee3a08
aaminha <dks4857@gmail.com>
2023-09-11 18:26:05 +0900

    fix(MainPage): change import order

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,9 @@
 import Advantage from "@/components/MainPage/Advantage";
 import ExpectedEffect from "@/components/MainPage/ExpectedEffect";
 import Hospital from "@/components/MainPage/Hospital";
-import SiteReview from "@/components/MainPage/SiteReview";
 import LearnMore from "@/components/MainPage/LearnMore";
 import MarketStatus from "@/components/MainPage/MarketStatus";
-
+import SiteReview from "@/components/MainPage/SiteReview";
 
 const MainPage = () => {
   return (
