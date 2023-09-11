7f2b87b9b38bf2f238eb3ec8cc2b8e01db736c01
aaminha <dks4857@gmail.com>
2023-09-11 18:19:13 +0900

    feat: add MarketStatus component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,13 @@
 import Advantage from "@/components/MainPage/Advantage";
-import Hospital from "@/components/MainPage/Hospital";
 import ExpectedEffect from "@/components/MainPage/ExpectedEffect";
 import Hospital from "@/components/MainPage/Hospital";
 import LearnMore from "@/components/MainPage/LearnMore";
+import MarketStatus from "@/components/MainPage/MarketStatus";
 
 const MainPage = () => {
   return (
     <>
+      <MarketStatus />
       <Advantage />
       <ExpectedEffect />
       <Hospital />
