8e059ad12f39ea03e406240faba1c1a2d3c83faa
hoyyChoi <hoyeon8165@gmail.com>
2023-09-12 00:10:40 +0900

    feat : Main <- carousel component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,14 @@
 import Advantage from "@/components/MainPage/Advantage";
+import Carousel from "@/components/MainPage/Carousel";
 import ExpectedEffect from "@/components/MainPage/ExpectedEffect";
 import Hospital from "@/components/MainPage/Hospital";
-import SiteReview from "@/components/MainPage/SiteReview";
 import LearnMore from "@/components/MainPage/LearnMore";
-
+import SiteReview from "@/components/MainPage/SiteReview";
 
 const MainPage = () => {
   return (
     <>
+      <Carousel />
       <Advantage />
       <ExpectedEffect />
       <SiteReview />
