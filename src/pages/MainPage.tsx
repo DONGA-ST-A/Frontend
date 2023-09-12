0f5ea9f0dca97df2b9b195d538e3c116721cfb9f
aaminha <dks4857@gmail.com>
2023-09-13 00:22:30 +0900

    remove: remove ScrollTab component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,14 +4,12 @@ import ExpectedEffect from "@/components/MainPage/ExpectedEffect";
 import Hospital from "@/components/MainPage/Hospital";
 import LearnMore from "@/components/MainPage/LearnMore";
 import MarketStatus from "@/components/MainPage/MarketStatus";
-import ScrollTab from "@/components/MainPage/ScrollTab";
 import SiteReview from "@/components/MainPage/SiteReview";
 
 const MainPage = () => {
   return (
     <>
       <Carousel />
-      <ScrollTab />
       <MarketStatus />
       <Advantage />
       <ExpectedEffect />
