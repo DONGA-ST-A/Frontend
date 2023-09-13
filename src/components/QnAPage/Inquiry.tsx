7493b1ed24d34d5b1dbcd8d7da5e39b432389c83
aaminha <dks4857@gmail.com>
2023-09-14 01:24:56 +0900

    feat: add Inquiry component

---
+++ src/components/QnAPage/Inquiry.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const Inquiry = () => {
+  return (
+    <Container>
+      <div>궁금증이 해결되지 않았다면?</div>
+      <button>1:1 문의하기</button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: var(--color_sub5);
+  text-align: center;
+  padding: 70px 0px;
+
+  div {
+    font-family: NotoSansMedium;
+    color: var(--color_sub3);
+    font-size: var(--text_body2);
+    padding-bottom: 16px;
+  }
+
+  button {
+    border: solid 1px;
+    border-color: var(--color_sub1);
+    border-radius: 36px;
+    background-color: var(--color_white);
+    font-family: NotoSansBold;
+    font-size: var(--text_body1);
+    color: var(--color_font);
+    padding: 12px 67px;
+  }
+`;
+
+export default Inquiry;
--- src/pages/QnAPage.tsx
+++ src/pages/QnAPage.tsx
@@ -1,17 +1,23 @@
+import React from "react";
+
 import styled from "styled-components";
 
+import Inquiry from "@/components/QnAPage/Inquiry";
 import QnACategoryBar from "@/components/QnAPage/QnACategoryBar";
 import { Inner } from "@/style/commonStyle";
 
 const QnAPage = () => {
   return (
-    <Inner>
-      <Banner>
-        <h1>QnA</h1>
-        <div>구매하시려는 제품에 대해 궁금한 점이 있으면 문의해주세요.</div>
-      </Banner>
-      <QnACategoryBar />
-    </Inner>
+    <React.Fragment>
+      <Inner>
+        <Banner>
+          <h1>QnA</h1>
+          <div>구매하시려는 제품에 대해 궁금한 점이 있으면 문의해주세요.</div>
+        </Banner>
+        <QnACategoryBar />
+      </Inner>
+      <Inquiry />
+    </React.Fragment>
   );
 };
 
