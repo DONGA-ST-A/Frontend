64d98b72047f92e42050224a8ebcf3f443b78814
aaminha <dks4857@gmail.com>
2023-09-17 04:20:43 +0900

    feat: add RnD component

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/CompanyPage/About";
 import Intro from "@/components/CompanyPage/Intro";
+import RnD from "@/components/CompanyPage/RnD";
 
 const CompanyPage = () => {
   return (
     <>
       <Intro />
       <About />
+      <RnD />
     </>
   );
 };
