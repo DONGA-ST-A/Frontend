ebfa225bbee5567b1ae452205d7ebfeadfb5c2b2
aaminha <dks4857@gmail.com>
2023-09-17 05:51:07 +0900

    feat: add LearnMore2 component

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -2,6 +2,7 @@ import About from "@/components/CompanyPage/About";
 import Business from "@/components/CompanyPage/Business";
 import History from "@/components/CompanyPage/History";
 import Intro from "@/components/CompanyPage/Intro";
+import LearnMore2 from "@/components/CompanyPage/LearnMore2";
 import RnD from "@/components/CompanyPage/RnD";
 
 const CompanyPage = () => {
@@ -12,6 +13,7 @@ const CompanyPage = () => {
       <RnD />
       <Business />
       <History />
+      <LearnMore2 />
     </>
   );
 };
