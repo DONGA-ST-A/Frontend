bcb972ae4c515a75f0e1ab2819ce86d36a82c187
hoyyChoi <hoyeon8165@gmail.com>
2023-09-17 18:02:01 +0900

    feat : add type Using gif

--- src/types/index.ts
+++ src/types/index.ts
@@ -80,3 +80,10 @@ export interface QnAData {
 }
 
 export type GetQnaResponse = GetDto<QnAData>;
+
+export interface UsingGif {
+  img: string;
+  gif: string;
+  text: string;
+  subText?: string;
+}
